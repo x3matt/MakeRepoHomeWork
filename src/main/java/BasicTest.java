e7f8a48addd5c59bb3b9efe0a0b98cc8391b05c5
x3matt <x3matt21@mail.ru>
2019-08-30 03:46:55 +0700

    Added solution

--- src/main/java/BasicTest.java
+++ src/main/java/BasicTest.java
@@ -1,33 +1,48 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import utils.FunctionalTest;
-import java.util.Scanner;
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import java.util.List;
+
+import static junit.framework.TestCase.*;
+
 public class BasicTest extends FunctionalTest {
 
     LoginPage loginPage = new LoginPage(driver);
     MainPage mainPage;
     CreatePage createPage;
     RepoListPage repoListPage = new RepoListPage(driver);
+    DeletePage deletePage = new DeletePage(driver);
 
-    Scanner scanner = new Scanner(System.in);
-    String user = "x3matt21@mail.ru";
-    String password;
+    String user = "your Email";
+    String password = "Your Password";
     String baseUrl = "https://www.github.com/login";
     String repoListURL = "https://github.com/x3matt?tab=repositories";
     String repoName = "MyRepo";
-    String userName= "x3matt";
+    String userName= "Your nickName";
     String repoDescr = "Some Valid Description";
+
+    //All tests passed
 
     @Before
     public void init(){
         PageFactory.initElements(driver,this);
     }
     @Test
+    public void loginTest(){
+        driver.get(baseUrl);
+        LoginPage page = new LoginPage(driver);
+        page.login("test","test");
+        assertEquals("Incorrect username or password.",driver.findElement(By.cssSelector("#js-flash-container > div > div")).getText());
+        page.login.clear();
+        page.password.clear();
+        page.login(user,password);
+        assertTrue(driver.findElement(By.cssSelector("body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-lg-flex > details > summary > img")).isDisplayed());
+    }
+    @Test
     public void createRepo(){
-        password = scanner.nextLine();
 
        driver.get(baseUrl);
        mainPage = loginPage.login(user,password);
@@ -42,5 +57,9 @@ public class BasicTest extends FunctionalTest {
 
        driver.get(repoListURL);
 
+       driver.get("https://github.com/"+userName+"/"+repoName);
+        deletePage.delete();
+        assertTrue(deletePage.buttonIsEnabled());
+        deletePage.pressDeleteButton();
     }
 }
--- src/main/java/DeletePage.java
+++ src/main/java/DeletePage.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.support.FindBy;
 import utils.PageObject;
 
 public class DeletePage extends PageObject {
-    @FindBy(css = "#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > nav > a:nth-child(8)")
+    @FindBy(xpath = "//*[@id=\"js-repo-pjax-container\"]/div[1]/nav/a[5]")
     WebElement settings;
     @FindBy(css = "#options_bucket > div.Box.Box--danger > ul > li:nth-child(4) > details > summary")
     WebElement deleteRepoButton;
-    @FindBy(css = "#options_bucket > div.Box.Box--danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > p > input")
+    @FindBy(css = "#options_bucket > div.Box.Box--danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow- auto > form > p > input")
     WebElement deleteButton;
     public DeletePage(WebDriver driver) {
         super(driver);
